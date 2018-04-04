12f010465725fc7069485bec6653f95920dc0c33
AdamDomagalsky <adadom@st.amu.edu.pl>
2018-04-04 12:18:01 +0200

    form unit tests (validation + submit with routing)

--- src/app/character-form/character-form.component.spec.ts
+++ src/app/character-form/character-form.component.spec.ts
@@ -1,12 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { Location } from '@angular/common';
+
 import { ReactiveFormsModule } from '@angular/forms';
-import {RouterTestingModule} from '@angular/router/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { CharacterFormComponent } from './character-form.component';
-import { CharactersService, Character } from '../services/characters.service';
+import { CharactersService } from '../services/characters.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
+import { CharacterFormComponent } from './character-form.component';
 
-describe('CharacterFormComponent', () => {
+describe('CharactercharacterFormComponent', () => {
   let component: CharacterFormComponent;
   let fixture: ComponentFixture<CharacterFormComponent>;
 
@@ -27,27 +29,48 @@ describe('CharacterFormComponent', () => {
     component.ngOnInit();
   });
 
-  it('form invalid when empty', () => {
+  it('characterForm invalid when empty', () => {
     expect(component.characterForm.valid).toBeFalsy();
   });
 
 
-  // it('name field validity', () => {
-  //   let name = component.characterForm.controls['name'];
-  //   expect(name.valid).toBeFalsy();
-  // });
+  it('name field validity', () => {
+    const name = component.characterForm.controls['name'];
+    name.setValue('TestnName');
+    const errors = name.errors || {};
+    expect(errors['reqired']).toBeFalsy();
+    expect(name.valid).toBeTruthy();
+  });
+
+  it('gender field validity', () => {
+    const gender = component.characterForm.controls['gender'];
+    gender.setValue('male');
+    const errors = gender.errors || {};
+    expect(errors['reqired']).toBeFalsy();
+    expect(gender.valid).toBeTruthy();
+  });
+
+  it('species field validity', () => {
+    const species = component.characterForm.controls['species'];
+    species.setValue('Dug');
+    const errors = species.errors || {};
+    expect(errors['reqired']).toBeFalsy();
+    expect(species.valid).toBeTruthy();
+  });
 
 
-  // it('name field validity', () => {
-  //   let errors = {};
-  //   let name = component.characterForm.controls['name'];
-  //   errors = name.errors || {};
-  //   expect(errors['required']).toBeTruthy();
-  // });
+  it('submitting a characterForm emits a character', fakeAsync(() => {
+    expect(component.characterForm.valid).toBeFalsy();
+    component.characterForm.controls['name'].setValue('SubmitTestName');
+    component.characterForm.controls['species'].setValue('Dug');
+    component.characterForm.controls['gender'].setValue('female');
+    expect(component.characterForm.valid).toBeTruthy();
+    component.characterForm.controls['homeworld'].setValue('RandomWOrld');
 
-
-
-
-
-
+    component.onSubmit();
+    // ROUTING TEST
+    tick(500);
+    const location = TestBed.get(Location);
+    expect(location.path()).toBe('');
+  }));
 });
